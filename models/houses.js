1f7f8b81daa63cdbbeb2e77301a2e7701d7b894a
S559328 <S559328@nwmissouri.edu>
2023-04-28 22:07:06 -0500

    added changes

--- models/houses.js
+++ models/houses.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose")
 const housesSchema = mongoose.Schema({
-    houses_color: String,
+    houses_color: {
+        type:String,
+        required: true,
+        match:/^[a-zA-Z]+$/
+    } ,
     houses_length: String,
-    houses_cost: String
+    houses_cost: {
+        type :String,
+        pattern: /^[a-z]+$/
+    }
 })
 module.exports = mongoose.model("houses",housesSchema)
